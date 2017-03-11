0c4e53341421a62a04380ff145572d8dc9bae2c5
Brian Knox <taotetek@gmail.com>
2017-03-11 17:00:43 -0500

    Problem: we need a build system

---
+++ src/echo_server.c
@@ -0,0 +1,47 @@
+/*  =========================================================================
+    echo_server - Echo server
+
+    Copyright (c) the Contributors as noted in the AUTHORS file.       
+    This file is part of echoz.                                        
+                                                                       
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.           
+    =========================================================================
+*/
+
+/*
+@header
+    echo_server - Echo server
+@discuss
+@end
+*/
+
+#include "echoz_classes.h"
+
+int main (int argc, char *argv [])
+{
+    bool verbose = false;
+    int argn;
+    for (argn = 1; argn < argc; argn++) {
+        if (streq (argv [argn], "--help")
+        ||  streq (argv [argn], "-h")) {
+            puts ("echo_server [options] ...");
+            puts ("  --verbose / -v         verbose test output");
+            puts ("  --help / -h            this information");
+            return 0;
+        }
+        else
+        if (streq (argv [argn], "--verbose")
+        ||  streq (argv [argn], "-v"))
+            verbose = true;
+        else {
+            printf ("Unknown option: %s\n", argv [argn]);
+            return 1;
+        }
+    }
+    //  Insert main code here
+    if (verbose)
+        zsys_info ("echo_server - Echo server");
+    return 0;
+}
